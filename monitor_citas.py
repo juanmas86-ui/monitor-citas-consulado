61fa33307826e90446ecd6f4d86aa8a7313e47ca
juanmas86-ui <juanmas86@gmail.com>
2026-06-27 20:01:17 -0300

    Update monitor_citas.py

--- monitor_citas.py
+++ monitor_citas.py
@@ -45,8 +45,11 @@ def enviar_email(asunto, cuerpo):
 def abrir_navegador():
     try:
         options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-blink-features=AutomationControlled')
         service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
         return driver
